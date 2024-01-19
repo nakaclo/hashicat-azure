9a2b48a556a5372f0b573cef7222585c950d0161
Your Name <you@example.com>
2024-01-19 02:36:30 +0000

    Added network module

--- network.tf
+++ network.tf
@@ -2,4 +2,5 @@ module "network" {
   source  = "app.terraform.io/chiptest/network/azurerm"
   version = "3.5.0"
   # insert required variables here
+  resource_group_name = azurerm_resource_group.myresourcegroup.name
 }
